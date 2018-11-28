dbbaed290233c15efe188a1715bed30238c7fa9f
asd848 <ampham@g.hmc.edu>
2018-11-27 19:25:52 -0800

    comments

--- IterSolver_V3.m
+++ IterSolver_V3.m
@@ -1,9 +1,11 @@
 % IterSolver_V3 that runs the iterative solver to find thickness for a desired
 % watt density
 
+% file = fopen('GlobalOutput.txt', 'wt');
+
 % System Parameters
 voltage = 115;
-tolerance = 1e-16;
+tolerance = 1;
 
 % Initial thickness guess
 i_thickness = 3000e-10*ones(10,10);
@@ -15,9 +17,14 @@ qjDes = 1.4/0.0254^2*ones(10,10);
 [out_thickness, qj_out] = busBarGeoSplitFull(voltage, i_thickness, qjDes);
 
 % Error calculation
-err = abs(sum(sum(out_thickness - i_thickness)));
-fprintf('The applied voltage is %d V \n', voltage)
-fprintf('The desired watt density is \n')
+% err = abs(sum(sum(out_thickness - i_thickness)));
+
+% fprintf(file, '%s %d \r\n', 'The applied voltage is', voltage)
+% fprintf(file, '%s \r\n', 'The desired watt density is')
+% dlmwrite('GlobalOutput.txt', qjDes, 'delimiter', ' ');
+
+fprintf('The applied voltage is %d \n', voltage)
+fprintf('The desired watt density is: \n')
 disp(qjDes)
 i = 0;
 
@@ -25,31 +32,54 @@ i = 0;
 % This was us trying a different approach of trying to vary thickness by a
 % proportion of the error between qj and qjDesried
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% err = qj_out - qjDes;
-% percent_err = err./qjDes * 100;
-% 
-% i_thickness(percent_err>10) = i_thickness(percent_err>10) ./abs(percent_err(percent_err>10)./1000000);
+err = qj_out - qjDes;
+percent_err = err./qjDes;
+err = norm(percent_err);
+% fprintf(file, 'The error is %d \n', err)
+fprintf('The error is %d \n', err)
+
+i_thickness(percent_err>0.1) = i_thickness(percent_err>0.1).*(1-percent_err(percent_err>0.1)./100);
 % disp(percent_err(percent_err>10));
 % pause
-% i_thickness(percent_err<-10) = i_thickness(percent_err<-10) .*abs(percent_err(percent_err<-10)./1000000);
+i_thickness(percent_err<-0.1) = i_thickness(percent_err<-0.1).*(1-percent_err(percent_err<-0.1)./100);
 % disp(percent_err(percent_err<-10))
 % pause
-%err = abs(sum(sum(out_thickness - i_thickness)));
-% err = norm(qj_out-qjDes);
+% err = abs(sum(sum(out_thickness - i_thickness)));
+% 
+% fprintf(file, '%s \r\n', 'The updated thickness is')
+% dlmwrite('GlobalOutput.txt', i_thickness, 'delimiter', ' ');
+fprintf('The updated thickness is: \n')
+disp(i_thickness)
 
+% pause
 % Begin iterative method
-while(abs(mean(mean(err))) > tolerance)
+while(err > tolerance)
     i = i +1;
-    i_thickness = out_thickness;
+%     i_thickness = out_thickness;
     [out_thickness, qj_out] = busBarGeoSplitFull(voltage, i_thickness, qjDes);
-    err = abs(sum(sum(out_thickness - i_thickness)));
-    fprintf('Iteration %d \n', i);
-    fprintf('The error is: %d\n', abs(mean(mean(err))));
-    fprintf('The thickness is: \n');
-    disp(i_thickness);
-    fprintf('The watt density is: \n');
-    disp(qj_out)
+    err = qj_out - qjDes;
+    percent_err = err./qjDes;
+
+    i_thickness(percent_err>0.1) = i_thickness(percent_err>0.1).*(1-percent_err(percent_err>0.1)./100);
+    i_thickness(percent_err<-0.1) = i_thickness(percent_err<-0.1).*(1-percent_err(percent_err<-0.1)./100);
+    err = norm(percent_err);
+%     err = abs(sum(sum(out_thickness - i_thickness)));
+%     fprintf(file, 'Iteration %d \n', i)
+%     fprintf(file, 'The error is %d \n', err)
+%     fprintf(file, 'The thickness is: \n');
+%     dlmwrite('GlobalOutput.txt', i_thickness, 'delimiter', ' ');
+%     disp(i_thickness);
+%     fprintf('The watt density is: \n');
+%     dlmwrite('GlobalOutput.txt', qj_out, 'delimiter', ' ');
+%     disp(qj_out)
     
+    fprintf('Iteration %d \n', i)
+    fprintf('The error is %d \n', err)
+    fprintf('The thickness is: \n')
+    disp(i_thickness)
+    fprintf('The watt density is: \n')
+    disp(qj_out)
+%     pause
 %%%%%%%%%%%%%%%%%%%%%    
 %    Other Approach
 %%%%%%%%%%%%%%%%%%%%%
@@ -61,6 +91,5 @@ while(abs(mean(mean(err))) > tolerance)
 %    
 %    i_thickness(i_thickness>8000e-10) = 8000e-10;
 %    i_thickness(i_thickness<300e-10) = 300e-10; 
-
-
 end
+% fclose(file)
