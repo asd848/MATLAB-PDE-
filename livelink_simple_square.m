408427b98748f98033a09d77dcb8883d43efdc13
asd848 <ampham@g.hmc.edu>
2019-02-12 18:32:36 -0800

    added interpolation

---
+++ livelink_simple_square.m
@@ -0,0 +1,35 @@
+model = mphload('simple_square.mph');
+mphgetproperties(model.geom('geom1'))
+
+figure;
+mphgeom(model, 'geom1', 'facelabels', 'on');
+
+figure;
+mphmesh(model);
+
+%%
+figure;
+voltage = mphplot(model, 'pg1');
+figure;
+volumetric_heating = mphplot(model, 'pg2');
+
+% model.component('comp1').func().create('int1', 'Interpolation');
+interp = model.component('comp1').func('int1')
+% model.component('comp1').func('int1').set('interp', 'linear');
+
+%% Commands so far
+%{
+To plot things:
+model.study gives you the child node std1
+model.study('std1').run executes the program
+
+The results are listed under several children, called 'pg1', 'pg2',etc.
+Plot these children by using the command mphplot(model,'pg1')
+
+Command chain for getting properties of boundary condition 
+mphgetproperties(model.physics('hteq#').feature('flux1'))
+
+Set the properties of the boundary conditions
+model.physics('hteq#').feature('flux1').set('q', 1, '#','g',1,'#')
+
+%}
