28d6a264ffffce851cdaa286dbf6850f787a8b27
asd848 <ampham@g.hmc.edu>
2019-03-07 19:24:17 -0800

    changes

--- livelink_simple_square.m
+++ livelink_simple_square.m
@@ -110,7 +110,7 @@ updated_delta_data = [xy_coords(1:2,:)' updated_delta];
 % Iteration counter
 i = 1;
 % Begin iterating
-while err > tol
+while err > tol %&& i < 80
     i = i + 1;
     % Grab current delta
     current_delta = updated_delta;
@@ -135,8 +135,10 @@ while err > tol
     percent_error = abs(qj_des-current_qj)./qj_des;
     %updated_delta(percent_error>0.01) = current_delta(percent_error>0.01) + current_delta(percent_error>0.01).*(current_qj(percent_error>0.01)./qj_des(percent_error>0.01) - 1)*alpha;
     % Update delta
-     updated_delta = current_delta + current_delta.*(current_qj./qj_des - 1)*alpha;
+     %updated_delta = current_delta + current_delta.*(current_qj./qj_des - 1)*alpha;
+     updated_delta = current_delta + ((qj_des-current_qj)/(230^2*10^6))*alpha;
      %Attempt to use relaxation method
+     
      updated_delta_matrix = reshape(updated_delta, [100, 100])';
      filtered_updated_delta_matrix = zeros(100, 100);
      for x = 1:100
@@ -144,10 +146,13 @@ while err > tol
              if x > 2 && x < 99 && y > 2 && y< 99
                  temp_cell_average = sum(sum(updated_delta_matrix(x-2:x+2, y-2:y+2)))/25;
                  filtered_updated_delta_matrix(x, y) = temp_cell_average;
-             elseif x < 3 && y > 2 && y <98
+             elseif x < 3 && y > 2 && y <99
                  r = 1 - x;
-                 sum(sum(updated_delta_matrix(x+r:x+2, y-2:y+2)))
-                 temp_cell_average = sum(sum(updated_delta_matrix(x+r:x+2, y-2:y+2)))/((3-r)*4);
+                 temp_cell_average = sum(sum(updated_delta_matrix(x+r:x+2, y-2:y+2)))/((3-r)*5);
+                 filtered_updated_delta_matrix(x, y) = temp_cell_average;
+             elseif y < 3 && x > 2 && x <99
+                 s = 1-y;
+                 temp_cell_average = sum(sum(updated_delta_matrix(x-2:x+2, y+s:y+2)))/((3-s)*5);
                  filtered_updated_delta_matrix(x, y) = temp_cell_average;
              elseif x < 3 && y < 3
                  r = 1 - x;
@@ -159,9 +164,13 @@ while err > tol
                  s = 100 - y;
                  temp_cell_average = sum(sum(updated_delta_matrix(x+r:x+2, y-2:y+s)))/((3-r)*(3+s));
                  filtered_updated_delta_matrix(x, y) = temp_cell_average;
-             elseif x > 98 && y > 2 && y <98
+             elseif x > 98 && y > 2 && y <99
                  r = 100 - x;
-                 temp_cell_average = sum(sum(updated_delta_matrix(x-2:x+r, y-2:y+2)))/((3+r)*4);
+                 temp_cell_average = sum(sum(updated_delta_matrix(x-2:x+r, y-2:y+2)))/((3+r)*5);
+                 filtered_updated_delta_matrix(x, y) = temp_cell_average;
+              elseif y > 98 && x > 2 && x <99
+                 s = 100 - y;
+                 temp_cell_average = sum(sum(updated_delta_matrix(x-2:x+2, y-2:y+s)))/((3+s)*5);
                  filtered_updated_delta_matrix(x, y) = temp_cell_average;
              elseif x > 98 && y < 3
                  r = 100 - x;
@@ -179,7 +188,6 @@ while err > tol
      
     updated_delta = reshape(filtered_updated_delta_matrix', [10000, 1]);
      
-     pause
     % Pulls the x, y, thickness data from the results
     updated_delta_data = [xy_coords(1:2,:)' updated_delta];
     % Writing udpated thickness data points to csv
